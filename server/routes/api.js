121b5d5eecb868714bc51c95ec6ec7742a93d7bb
SixMiles <wangchu0211@gmail.com>
2020-06-29 13:34:42 -0400

    create events and special events api for testing

--- server/routes/api.js
+++ server/routes/api.js
@@ -45,7 +45,64 @@ router.post('/login',(req,res)=>{//make a link to the localhost
     })
 })
 
+router.get('/events',(req,res)=>{
+    let events = [{
+        "_id":"1",
+        "name":"auto expo",
+        "description":"lorem ipsum",
+        "date":"2012-04-23T18:25:43.511Z"
+    },
+    {
+        "_id":"2",
+        "name":"auto expo",
+        "description":"lorem ipsum",
+        "date":"2012-04-23T18:25:43.511Z"
+    },
+    {
+        "_id":"3",
+        "name":"auto expo",
+        "description":"lorem ipsum",
+        "date":"2012-04-23T18:25:43.511Z"
+    },
+    {
+        "_id":"4",
+        "name":"auto expo",
+        "description":"lorem ipsum",
+        "date":"2012-04-23T18:25:43.511Z"
+    }
 
+]
+res.json(events)}
+)
+
+router.get('/special',(req,res)=>{
+    let events = [{
+        "_id":"1",
+        "name":"auto expo",
+        "description":"lorem ipsum",
+        "date":"2012-04-23T18:25:43.511Z"
+    },
+    {
+        "_id":"2",
+        "name":"auto expo",
+        "description":"lorem ipsum",
+        "date":"2012-04-23T18:25:43.511Z"
+    },
+    {
+        "_id":"3",
+        "name":"auto expo",
+        "description":"lorem ipsum",
+        "date":"2012-04-23T18:25:43.511Z"
+    },
+    {
+        "_id":"4",
+        "name":"auto expo",
+        "description":"lorem ipsum",
+        "date":"2012-04-23T18:25:43.511Z"
+    }
+
+]
+res.json(events)})
 
 module.exports = router
 
