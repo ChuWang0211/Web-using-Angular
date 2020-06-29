475e85a11827e83cd50682f5902fcdd6b75f558b
SixMiles <wangchu0211@gmail.com>
2020-06-29 10:18:41 -0400

    connect database to server
    uses MongoDB as database, connect database to server. applied Mongoose to enable the input and output to the database from server

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,6 +1,16 @@
 const express = require('express')
 const router = express.Router()
 
+const mongoose = require('mongoose')
+//link to my database in MongoDB with username and password
+const connectionString = "mongodb+srv://AngularUser1:AngularUser1@angularsdatabase-kdtrl.azure.mongodb.net/<dbname>?retryWrites=true&w=majority"
+
+mongoose.connect(connectionString,err =>{//connect and action if error occur
+    if(err){
+        console.error('Error'+err)
+    }else{console.log('connected to mongodb')}
+})
+
 router.get('/',(req, res) => {
     res.send('From API route')
 })
--- server/server.js
+++ server/server.js
@@ -1,13 +1,14 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 
-const PORT = 3000
+const PORT = 3000//localhost address,can be numbers
 const app = express()
-const api = require('./routes/api')
+const api = require('./routes/api')//restore the file address
 
 app.use(bodyParser.json())
 
-app.use('/api', api)
+app.use('/api', api)//add api page to server
+
 app.get('/', function(req, res){
     res.send('Hello form server')
 })
