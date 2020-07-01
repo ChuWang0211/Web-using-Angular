bbdbd1e79f68c750b175727e487ae9935154dfc4
SixMiles <wangchu0211@gmail.com>
2020-07-01 13:28:38 -0400

    connect front end login to back end login

--- ngApp/src/app/auth.service.ts
+++ ngApp/src/app/auth.service.ts
@@ -10,9 +10,14 @@ import {HttpClient} from '@angular/common/http'
 
 export class AuthService { // this is a AuthService class
   private _registerUrl= "http://localhost:3000/api/register" // this is a variable that stored the backend API url
+  private _loginUrl= "http://localhost:3000/api/login"
   constructor(private http:HttpClient) { }
   registerUser(user){ // this method which needs a user object(json) prameter (user object is the email and the password)
     return this.http.post<any>(this._registerUrl,user)// in the function, we make a post request and returned the observerable. the first argument is the url, and the second argument is the user object/json 
   } 
+
+  loginUser(user){
+    return this.http.post<any>(this._loginUrl,user)
+  }
 }
   
--- ngApp/src/app/login/login.component.ts
+++ ngApp/src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {AuthService} from '../auth.service'; 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -8,13 +8,17 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
   
   loginUserData={email: '', password: ''};
-  constructor() { }
+  constructor(private _auth:AuthService) { }
 
   ngOnInit(): void {
   
   }
   loginUser(){
-    console.log(this.loginUserData)
-  }
+    this._auth.loginUser(this.loginUserData)
+    .subscribe( // uses observiable //when using ths obserable, we either get a response or error
+      res => console.log(res),// if get response, we can use the response 
+      err =>console.log(err)// if get error, when show something to indicate the error
+    )
+    }
 
 }
